0267455e86f00c18d19f6cad3869003776e80974
SeannieLim <161107124+SeannieLim@users.noreply.github.com>
2024-04-16 23:38:35 +1000

    Added Images to Assets for Skills
    Working on adding images for skills

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Components/Navbar.jsx";
 import Home from './Components/Home.jsx';
 import About from "./Components/About.jsx";
+import Skills from "./Components/Skills.jsx"
 function App(){
   return(
     <>
     <Navbar/>
     <Home/>
     <About/>
+    <Skills/>
   </>
   )
 }
--- Portfolio/src/Components/Skills.jsx
+++ Portfolio/src/Components/Skills.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import Javascript from '../assets/javascript.png'
+import Node from '../assets/node.png'
+import ReactJs from '../assets/react.png'
+import Tailwind from '../assets/tailwind.png'
+
 const Skills = () => {
   return (
-    <div>
-      
+    <div name="skills" className="bg-[#0a192f] text-gray-300">
+
+        {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+            <p className="text-4xl font-bold inline border-b-4 border-green-600">Experience</p>
+            <p className="py-4">These are the technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py--8">
+            <img className="w-20 mx-auto"src={HTML} alt="HTML icon"/>
+            <p>HTML</p>
+        </div>
+      </div>
     </div>
   )
 }
