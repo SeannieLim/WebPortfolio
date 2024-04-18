0e64cda303de643e50ff72fc2fa78beba3f28d86
SeannieLim <SeanLHM@outlook.com>
2024-04-18 16:47:45 +1000

    Navbar and Resume Adjustments
    Reduce size for logo for navbar and edited the viewport for resume page

--- Portfolio/src/Components/Navbar.jsx
+++ Portfolio/src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 mt-2 marker:bg-[#0a192f] text-gray-300'>
         <div>
-            <img src={logo} alt="Logo Image" style={{width: '70px'}} />
+            <img src={logo} alt="Logo Image" style={{width: '50px'}} />
         </div>
         {/* Menu */}
         <nav className="text-xl">
--- Portfolio/src/Components/Work.jsx
+++ Portfolio/src/Components/Work.jsx
@@ -16,13 +16,13 @@ import { Link } from 'react-scroll';
 const Work = () => {
   return (
     <>
+    <div className="bg-[#0a192f] min-h-screen">
     <Navbar/>
     <div className="w-full h-screen bg-[#0a192f] text-gray-300">
          <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#bec5db] mx-auto'>My Resume</h1>
-
             <Typical className="text-[#C7F6C7] text-3xl py-4 max-w-[700px] mx-auto"
-        steps={['Welcome', 1000,'Please take a look!', 500 ]}
+        steps={['Welcome', 1000,'Please take a look!', 500]}
         loop={Infinity}
         wrapper="p"
       />
@@ -43,9 +43,9 @@ const Work = () => {
             </div>
     </div>
 
-    <div className="w-full h-screen bg-[#0a192f]">
+    <div className="w-full min-h-screen bg-[#0a192f]">
     <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-    <div id="resume" className="container mx-auto py-8 px-4 hover:scale-110 duration-500">
+    <div id="resume" className="container mx-auto py-8 px-4">
         <div className="bg-white p-6 rounded-lg shadow-lg">
         <h1 className="text-3xl font-semibold text-slate-950">Sean Lim</h1>
             
@@ -91,58 +91,11 @@ const Work = () => {
 
           </div>
         </div>
-    
-        <div className="mt-40">
-            <p className="text-4xl font-bold inline border-b-4 border-green-600">Experience</p>
-            <p className="py-4 text-2xl mt-2">These are the technologies I've worked with</p>
-        </div>
-
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py--8">
-
-        <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-          <img className="w-20 mx-auto"src={HTML} alt="HTML icon"/>
-            <p className="my-4">HTML</p>
-          </div> 
-
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-          <img className="w-20 mx-auto"src={CSS} alt="Css icon"/>
-            <p className="my-4">CSS</p>
-          </div> 
-
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-          <img className="w-20 mx-auto"src={Javascript} alt="Js icon"/>
-            <p className="my-4">Javascript</p>
-          </div> 
-
-
-      
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-          <img className="w-20 mx-auto"src={Node} alt="Node icon"/>
-            <p className="my-4">Node</p>
-          </div> 
-
-
-       
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-          <img className="w-20 mx-auto"src={ReactJs} alt="ReactJs icon"/>
-            <p className="my-4">ReactJS</p>
-          </div> 
-
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-          <img className="w-20 mx-auto"src={Tailwind} alt="Tailwind icon"/>
-            <p className="my-4">Tailwind</p>
-          </div> 
-
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-          <img className="w-20 mx-auto"src={Github} alt="Github icon"/>
-            <p className="my-4">Github</p>
-          </div>
-          
-          </div>
-
+        <Skills/>
 </div>
     
 </div>
+    </div>
     </div>
     </>
   )
