99f9ab5e726e8a47a9a3db84ee666f9807d0aaf6
SeannieLim <SeanLHM@outlook.com>
2024-04-27 19:02:12 +1000

    Update NavBar
    Update Navbar (Change the name of the playground to portfolio)

--- Portfolio/src/Components/Navbar.jsx
+++ Portfolio/src/Components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/work">Resume</Link></li>
-                <li><Link to="/Playground">Playground</Link></li>
+                <li><Link to="/Playground">Portfolio</Link></li>
             </ul>
         </nav>
         {/* Hamburger Menu */}
